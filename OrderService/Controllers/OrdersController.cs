212e056f6f369822f9f66baace3fb1c59c2dce1f
Sureka UCA <sureka@unicornconnectedapps.com>
2025-02-13 17:45:11 +0530

    Add project files.

---
+++ OrderService/Controllers/OrdersController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace OrderService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetOrders()
+        {
+            return Ok(new List<string> { "Order1", "Order2" });
+        }
+    }
+
+}
+
---
+++ ProductService/Controllers/ProductsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductService.Model;
+
+namespace ProductService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(new List<string> { "Product1", "Product2" });
+        }
+    }
+}
